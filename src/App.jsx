79df3a19db7041218d302ae4d0c0678185761ce2
VitaliiTk <tka4uk.gt41@gmail.com>
2024-12-27 22:37:51 +0600

    add some props to Pokemon cards

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,15 @@ import './App.css'
 import PokemonCard from './PokemonCard'
 import Protiv from './Protiv'
 
+const name1 = 'Pikachu'
+const name2 = 'Charmander'
+
 function App() {
   return (
     <>
-      <PokemonCard />
+      <PokemonCard name={name1} />
       <Protiv />
-      <PokemonCard />
+      <PokemonCard name={name2}/>
     </>
   )
 }
--- src/PokemonCard.jsx
+++ src/PokemonCard.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './PokemonCard.css'
 
-export default function PokemonCard() {
+export default function PokemonCard({ name }) {
   const randomNumber = Math.floor(Math.random() * 1025) + 1
   const pokemonImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${randomNumber}.png`
 
@@ -9,7 +9,7 @@ export default function PokemonCard() {
 
   return (
     <div className="PokemonCard">
-      <h2>Pokemon</h2>
+      <h2>{name}</h2>
       <h3>#{randomNumber}</h3>
       <img src={pokemonImage} alt="pokemon" />
       <div className="card">
