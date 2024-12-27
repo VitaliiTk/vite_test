f83d161bd4f6e911e50d7a6ab42d94614e0589a8
VitaliiTk <tka4uk.gt41@gmail.com>
2024-12-27 10:53:08 +0600

    add reload react component on btn click

--- src/PokemonCard.jsx
+++ src/PokemonCard.jsx
@@ -5,14 +5,14 @@ export default function PokemonCard() {
   const randomNumber = Math.floor(Math.random() * 1025) + 1
   const pokemonImage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${randomNumber}.png`
 
-  const [count, setCount] = useState(0)
+  const [reload, setReload] = useState(false)
 
   return (
     <div className="PokemonCard">
       <h1>Pokemon Card #{randomNumber}</h1>
       <img src={pokemonImage} alt="pokemon" />
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>Click</button>
+        <button onClick={() => setReload((prev) => !prev)}>Click</button>
       </div>
     </div>
   )
